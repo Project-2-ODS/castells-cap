594a02f16756a98089520ae7a7200216274ba1b2
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-22 16:00:45 +0200

    first commit

---
+++ src/main/java/com/example/castells_cap/CastellsCapApplication.java
@@ -0,0 +1,13 @@
+package com.example.castells_cap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastellsCapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastellsCapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/castells_cap/CastellsCapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.castells_cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastellsCapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
