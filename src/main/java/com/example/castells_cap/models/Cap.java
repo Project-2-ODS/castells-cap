2a3cbb0dc17383f0fdf7192e1fd796fa8fcbf10a
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-27 20:15:24 +0200

    model done

--- src/main/java/com/example/castells_cap/models/Cap.java
+++ src/main/java/com/example/castells_cap/models/Cap.java
@@ -1,4 +1,24 @@
 package com.example.castells_cap.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String email;
 }
